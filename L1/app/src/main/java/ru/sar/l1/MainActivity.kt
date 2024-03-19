1352204dcda48dbded789e48a47219a44b558dd6
Aleksey S <m1330563@gmail.com>
2024-03-19 16:42:50 +0400

    ch. text

--- L1/app/src/main/java/ru/sar/l1/MainActivity.kt
+++ L1/app/src/main/java/ru/sar/l1/MainActivity.kt
@@ -2,10 +2,13 @@ package ru.sar.l1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.test_text).text="Changed text"
     }
 }
