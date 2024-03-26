a853ac2ba304f07de3b608890f897306a1eb421b
Aleksey S <m1330563@gmail.com>
2024-03-26 09:22:57 +0400

    landscape and vert. view - saveInstantState/RestoreInstantState

--- L1/app/src/main/java/ru/sar/l1/MainActivity.kt
+++ L1/app/src/main/java/ru/sar/l1/MainActivity.kt
@@ -1,16 +1,60 @@
 package ru.sar.l1
 
-import android.app.Application
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+const val KEY_COUNTER = "count"
 
 class MainActivity : AppCompatActivity() {
+    private var currentCounter = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("TegTest", "mainActivity onCreate")
+        Log.d("Test", "mainActivity onCreate")
+        val plusButton: Button = findViewById(R.id.plusButton)
+
+//        if (savedInstanceState != null) currentCounter=savedInstanceState.getInt(KEY_COUNTER)
+        savedInstanceState?.getInt(KEY_COUNTER)?.let { counter ->
+            currentCounter = counter}
+
+        val counterTextView: TextView = findViewById(R.id.counterTextView)
+        counterTextView.text = currentCounter.toString()
+
+        plusButton.setOnClickListener {
+            currentCounter++
+            counterTextView.text = currentCounter.toString()
+        }
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("Test", "Destroy main activity")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        Log.d("Test", "save instance")
+        super.onSaveInstanceState(outState)
+        //outState.putInt(KEY_COUNTER, currentCounter )
+        outState.putInt(KEY_COUNTER,currentCounter)
+
+    }
+
+//    override fun onRestoreInstanceState(
+//        savedInstanceState: Bundle
+//    ) {
+//        Log.d("Test", "Load instance")
+//        super.onRestoreInstanceState(savedInstanceState)
+//
+//        currentCounter = savedInstanceState.getInt(KEY_COUNTER)
+//        val counterTextView: TextView = findViewById(R.id.counterTextView)
+//        counterTextView.text = currentCounter.toString()
+//
+//
+//
+//    }
 }
 
--- L1/app/src/main/java/ru/sar/l1/MyApplication.kt
+++ L1/app/src/main/java/ru/sar/l1/MyApplication.kt
@@ -2,10 +2,14 @@ package ru.sar.l1
 
 import android.app.Application
 import android.util.Log
-
+var cnt=0
 class MyApplication: Application() {
+//    var cnt=0
     override fun onCreate() {
         super.onCreate()
         Log.d("MyApplication", "MyApplication onCreate the testing")
+    }
+    fun inc() {
+        cnt++
     }
 }
