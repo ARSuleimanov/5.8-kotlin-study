20477479a042401f3b0dfe0ed30d4fb046b35fed
Aleksey S <m1330563@gmail.com>
2024-03-27 11:20:25 +0400

    log life style activity

--- L1/app/src/main/java/ru/sar/l1/MainActivity.kt
+++ L1/app/src/main/java/ru/sar/l1/MainActivity.kt
@@ -3,58 +3,56 @@ package ru.sar.l1
 import android.os.Bundle
 
 import android.util.Log
-import android.widget.Button
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
-const val KEY_COUNTER = "count"
+const val LOG_TAG = "activity"
 
 class MainActivity : AppCompatActivity() {
-    private var currentCounter = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("Test", "mainActivity onCreate")
-        val plusButton: Button = findViewById(R.id.plusButton)
 
-//        if (savedInstanceState != null) currentCounter=savedInstanceState.getInt(KEY_COUNTER)
-        savedInstanceState?.getInt(KEY_COUNTER)?.let { counter ->
-            currentCounter = counter}
+        Log.d(LOG_TAG, "onCreate")
+    }
 
-        val counterTextView: TextView = findViewById(R.id.counterTextView)
-        counterTextView.text = currentCounter.toString()
 
-        plusButton.setOnClickListener {
-            currentCounter++
-            counterTextView.text = currentCounter.toString()
-        }
+    override fun onStart() {
+        super.onStart()
+        Log.d(LOG_TAG, "onStart")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(LOG_TAG, "onRestart")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d(LOG_TAG, "onResume")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.d(LOG_TAG,"onSaveInstanceState")
+    }
+
+
+    override fun onPause() {
+        super.onPause()
+        Log.d (LOG_TAG,"onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(LOG_TAG,"onStop")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.d("Test", "Destroy main activity")
+        Log.d(LOG_TAG, "onDestroy")
     }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        Log.d("Test", "save instance")
-        super.onSaveInstanceState(outState)
-        //outState.putInt(KEY_COUNTER, currentCounter )
-        outState.putInt(KEY_COUNTER,currentCounter)
 
-    }
 
-//    override fun onRestoreInstanceState(
-//        savedInstanceState: Bundle
-//    ) {
-//        Log.d("Test", "Load instance")
-//        super.onRestoreInstanceState(savedInstanceState)
-//
-//        currentCounter = savedInstanceState.getInt(KEY_COUNTER)
-//        val counterTextView: TextView = findViewById(R.id.counterTextView)
-//        counterTextView.text = currentCounter.toString()
-//
-//
-//
-//    }
+
 }
 
