a1d195bf3df275341cbeb675c9d63d2dc4d0fd08
Aleksey S <m1330563@gmail.com>
2024-04-13 11:24:16 +0400

    temporary commit, please kill it. Purpose for this commit - transfer project to anothher PC

--- L1/app/src/main/java/ru/sar/l1/MainActivity.kt
+++ L1/app/src/main/java/ru/sar/l1/MainActivity.kt
@@ -3,9 +3,12 @@ package ru.sar.l1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+//        val transaction= supportFragmentManager.beginTransaction().add(R.id.TitleContainer,TitleNews(),"Title Fragment")
+//        transaction.commit()
     }
 }
