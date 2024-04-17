00a8c82afbc30ba8e9af50e3979c41594800e09f
Aleksey <arsuleimanov@outlook.com>
2024-04-17 17:54:57 +0400

    hw 10 is ready, tested. But need code refactoring

--- L1/app/src/main/java/ru/sar/l1/TheNewsFragment.kt
+++ L1/app/src/main/java/ru/sar/l1/TheNewsFragment.kt
@@ -1,5 +1,6 @@
 package ru.sar.l1
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -11,51 +12,77 @@ import android.widget.TextView
 const val TAG = "number"
 
 class TheNewsFragment : Fragment() {
-    private var param: Int? = null
 
+    private var idNews: Int? = null
+    private var menuItemColorBefore = Color.BLACK
 
-    override fun onDetach() {
-        super.onDetach()
-        NewsList.forwardStack.add(param?:100)
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        /**
+         * memorize color of menu item in the TitleFragment
+         */
         arguments?.let {
-            param = it.getInt(TAG)
+            idNews = it.getInt(TAG)
+            val fragment =
+                parentFragmentManager.findFragmentByTag("Title Fragment") as? TitleNewsFragment
+            val textView = fragment?.listOfTitleTextView?.get(idNews!!)
+            menuItemColorBefore = textView?.currentTextColor!!
         }
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        view.findViewById<TextView>(R.id.NewsTitle).text=NewsList.news[param?: return@onViewCreated].title
-        view.findViewById<TextView>(R.id.NewsAuthor).text=NewsList.news[param?: return@onViewCreated].author
-        view.findViewById<TextView>(R.id.NewsContent).text=NewsList.news[param?: return@onViewCreated].content
-
-    }
-
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_the_news, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<TextView>(R.id.NewsTitle).text =
+            NewsList.news[idNews ?: return@onViewCreated].title
+        view.findViewById<TextView>(R.id.NewsAuthor).text =
+            NewsList.news[idNews ?: return@onViewCreated].author
+        view.findViewById<TextView>(R.id.NewsContent).text =
+            NewsList.news[idNews ?: return@onViewCreated].content
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        //return the old color for the menu item
+        val fragment =
+            parentFragmentManager.findFragmentByTag("Title Fragment") as? TitleNewsFragment
+        val textView = idNews?.let { fragment?.listOfTitleTextView?.get(it) }
+        if (textView != null) {
+            textView.setTextColor(menuItemColorBefore)
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        //push to stack fragments for navigation in the future
+        NewsList.forwardStack.add(idNews ?: 100)
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        val fragment =
+            parentFragmentManager.findFragmentByTag("Title Fragment") as? TitleNewsFragment
+        fragment?.highLightItem(idNews ?: return)
+    }
+
+
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param Parameter 1.
-         * @return A new instance of fragment TheNewsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
+
         @JvmStatic
-        fun newInstance(param: Int) =
-            TheNewsFragment().apply {
-                arguments = Bundle().apply {
-                    putInt(TAG, param)
-                }
+        fun newInstance(param: Int) = TheNewsFragment().apply {
+            arguments = Bundle().apply {
+                putInt(TAG, param)
             }
+        }
     }
 }
